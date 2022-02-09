975dcae138e8a75c74cd120e5ff6a1a9d192f26f
Mariana1523 <79233254+Mariana1523@users.noreply.github.com>
2022-02-08 23:57:04 -0300

    Update meio.pl

--- meio.pl
+++ meio.pl
@@ -1,1 +1,15 @@
 
+ordenar(L,LO):- trocar(L,L1),!,  ordenar(L1,LO).
+ordenar(LO,LO).
+trocar([X,Y|C],[Y,X|C]):- maior(X,Y).
+trocar([Z|C],[Z|C1]):- trocar(C,C1).
+maior(A,B):-A>B.
+
+tiraUltimo([_],[]).
+tiraUltimo([C|L],[C|LU]):- tiraUltimo(L,LU).
+
+mei([M],M).
+mei([P,U],M):- M is (P+U)/2.
+mei([_|L],Mei):- tiraUltimo(L,LU), mei(LU,M1),Mei is M1.
+
+meio(L,R):- ordenar(L,LO), mei(LO,R).
