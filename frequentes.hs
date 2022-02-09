f005dde80e4cf78e489c270ba90f1cd293523203
Mariana1523 <79233254+Mariana1523@users.noreply.github.com>
2022-02-08 23:59:23 -0300

    Update frequentes.hs

--- frequentes.hs
+++ frequentes.hs
@@ -1,11 +1,11 @@
 frequentes :: [Int] -> [Int] 
 frequentes [] = []
-frequentes x
+frequentes x  
   | g == r = []
   | otherwise = reposta
   where
     g = length reposta
-    r = auxQtdNumero(ordenar x)
+    r = auxQtdNumero(ordenar x) 
     reposta = comparar(ordenar2(compactar(ordenar x))) 0
 
 compactar :: [Int] -> [(Int, Int)]
@@ -13,27 +13,28 @@ compactar []  = []
 compactar (x : xs) = (length(contador x xs) + 1, x) : compactar xs 
 
 contador :: Int -> [Int] -> [Int]
-contador  [] = []
+contador _ [] = []
 contador a (x : xs) 
-    | a == x = [a] ++ contador x xs
-    | a /= x =  []
+    | a == x = [a] ++ contador x xs   
+    | a /= x =  []  
     | otherwise  = []
 
 comparar :: [(Int, Int)] -> Int -> [Int]
-comparar []  = []
+comparar [] _ = []
 comparar ((x, y) : xs) m
-    | x >= m = [y] ++ comparar xs x
-    | otherwise = []
-
+    | x >= m = [y] ++ comparar xs x   
+    | otherwise = []     
+-- l: lista 
+-- ll : Lista de lista
 
 auxQtdNumero :: [Int] -> Int 
 auxQtdNumero (x:xs)  = qtdNumero xs x 
 
 qtdNumero :: [Int] -> Int  -> Int
-qtdNumero [] _ = 0
+qtdNumero [] _ = 1
 qtdNumero (x: xs) b 
-    | b == x = qtdNumero xs b 
-    | otherwise = 1 + qtdNumero xs b 
+    | b /= x = 1 + qtdNumero xs x     
+    | otherwise =  qtdNumero xs x 
 
 ordenar :: [Int] -> [Int] 
 ordenar [] = []
@@ -42,3 +43,4 @@ ordenar (a : x) = ordenar[b | b <- x, b <= a] ++ [a] ++ ordenar[b | b <- x, b > 
 ordenar2 ::  [(Int, Int)] ->  [(Int, Int)]
 ordenar2 [] = []
 ordenar2 (a : x) = ordenar2[b | b <- x, b > a] ++ [a] ++ ordenar2[b | b <- x, b <= a]
+
