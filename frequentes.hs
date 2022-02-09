9cabf229a9f3552ffa7dbc04c3b8cd279f3ddce5
Mariana1523 <79233254+Mariana1523@users.noreply.github.com>
2022-02-08 23:58:35 -0300

    Update frequentes.hs

--- frequentes.hs
+++ frequentes.hs
@@ -1,6 +1,6 @@
 frequentes :: [Int] -> [Int] 
 frequentes [] = []
-frequentes x  
+frequentes x
   | g == r = []
   | otherwise = reposta
   where
@@ -13,19 +13,18 @@ compactar []  = []
 compactar (x : xs) = (length(contador x xs) + 1, x) : compactar xs 
 
 contador :: Int -> [Int] -> [Int]
-contador _ [] = []
+contador  [] = []
 contador a (x : xs) 
-    | a == x = [a] ++ contador x xs   
-    | a /= x =  []  
+    | a == x = [a] ++ contador x xs
+    | a /= x =  []
     | otherwise  = []
 
 comparar :: [(Int, Int)] -> Int -> [Int]
-comparar [] _ = []
+comparar []  = []
 comparar ((x, y) : xs) m
-    | x >= m = [y] ++ comparar xs x   
-    | otherwise = []     
--- l: lista 
--- ll : Lista de lista
+    | x >= m = [y] ++ comparar xs x
+    | otherwise = []
+
 
 auxQtdNumero :: [Int] -> Int 
 auxQtdNumero (x:xs)  = qtdNumero xs x 
@@ -43,15 +42,3 @@ ordenar (a : x) = ordenar[b | b <- x, b <= a] ++ [a] ++ ordenar[b | b <- x, b > 
 ordenar2 ::  [(Int, Int)] ->  [(Int, Int)]
 ordenar2 [] = []
 ordenar2 (a : x) = ordenar2[b | b <- x, b > a] ++ [a] ++ ordenar2[b | b <- x, b <= a]
-
--- repete(N, [X,Y|Zs], Ls) :- X = Y, W is N + 1, repete(W, [Y|Zs], Ls).
--- repete(N, [X,Y|Zs], [[W, X]|Ls]) :- X \= Y, W is N + 1, repete(0, [Y|Zs], Ls).
-
-
--- quantidade :: a -> [a] -> [a]
--- quantidade _ [] = []
--- quantidade e (x : xs)
---   | e x = x : quantidade e xs
---   | otherwise = quantidade e xs
-
---   --Frequentes [c|L] = compactar [c|L], frequentes [L]
